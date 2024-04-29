bc2c31ffd5ca255d0ebf58692b5301acf6e9d1ee
ani1210l <122383018+ani1210l@users.noreply.github.com>
2024-04-28 20:51:08 -0500

    Excepciones

--- src/main/java/com/example/faan/mongo/Controller/WebSocketPublicacionController.java
+++ src/main/java/com/example/faan/mongo/Controller/WebSocketPublicacionController.java
@@ -1,6 +1,7 @@
 package com.example.faan.mongo.Controller;
 
 import com.example.faan.mongo.modelos.Publicacion;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -13,5 +14,11 @@ public class WebSocketPublicacionController {
     public Publicacion nuevaPublicacion(Publicacion publicacion) {
         // Procesar la nueva publicación
         return publicacion;
+    }
+
+    @MessageExceptionHandler
+    public void handleException(Throwable exception) {
+        // Maneja la excepción aquí, puedes registrarla, enviar una respuesta específica, etc.
+        System.out.println("Se produjo una excepción en el controlador de publicaciones WebSocket: " + exception.getMessage());
     }
 }
