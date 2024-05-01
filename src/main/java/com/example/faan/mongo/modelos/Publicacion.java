703a9121344ce2eb02509d10bb16fa9641ebf8f2
ani1210l <122383018+ani1210l@users.noreply.github.com>
2024-05-01 11:20:11 -0500

    Fech-sin hora2.4

--- src/main/java/com/example/faan/mongo/modelos/Publicacion.java
+++ src/main/java/com/example/faan/mongo/modelos/Publicacion.java
@@ -5,13 +5,12 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigInteger;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate; // Importa la clase LocalDate
 
 @Data
 @Builder
 @NoArgsConstructor
-//@AllArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @Document(collection = "Publicaciones")
@@ -25,37 +24,8 @@ public class Publicacion {
     private TipoAnimal tipoAnimal;
     private TipoPublicacion tipoPublicacion;
     private String descripcionEspecifica;
-    private Date fecha; // Mantenemos la fecha como un objeto Date
+    private LocalDate fecha; // Cambiado a LocalDate
     private String ubicacion;
     private Boolean estadoRescatado;
     private byte[] foto;
-
-    // Constructor Fecha
-
-
-
-    public Publicacion(BigInteger id, String nombre, String raza, String sexo, TipoAnimal tipoAnimal, TipoPublicacion tipoPublicacion, String descripcionEspecifica, Date fecha, String ubicacion, Boolean estadoRescatado, byte[] foto) {
-        this.id = id;
-        this.nombre = nombre;
-        this.raza = raza;
-        this.sexo = sexo;
-        this.tipoAnimal = tipoAnimal;
-        this.tipoPublicacion = tipoPublicacion;
-        this.descripcionEspecifica = descripcionEspecifica;
-        this.fecha = fecha;
-        this.ubicacion = ubicacion;
-        this.estadoRescatado = estadoRescatado;
-        this.foto = foto;
-    }
-
-    // Método para obtener la fecha formateada como "yyyy-MM-dd"
-    public String getFechaSinHora() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        return dateFormat.format(fecha);
-    }
-
-    // Setter para la fecha
-    public void setFecha(Date fecha) {
-        this.fecha = fecha;
-    }
 }
